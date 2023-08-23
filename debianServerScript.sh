31cdd2be395a302fd7ca587853d67f10107f1ea2
GOKU <117481463+poyo20@users.noreply.github.com>
2023-08-23 20:41:05 +0200

    First script

---
+++ debianServerScript.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install neofetch htop ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo docker volume create yacht
+sudo docker run -d -p 8000:8000 -v --name yacht --restart unless-stopped /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+reboot
